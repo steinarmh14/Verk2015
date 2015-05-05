71a3a094d0004cef6349e86fa32259cf463b2371
einarr14 <einarr14@ru.is>
2015-05-05 20:50:29 +0000

    oklarad followerfall

--- FeedItsolution/FeedIt/Service/FollowerService.cs
+++ FeedItsolution/FeedIt/Service/FollowerService.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FeedIt.Models;
 
 namespace FeedIt.Service
 {
     public class FollowerService
     {
+        public void addFollower(int followerID, int followingID)
+        {
+
+        }
+        public void removeFollower(int followerID, int followingID)
+        {
+
+        }
+        public List<ApplicationUser> getFollowers(int userID)
+        {
+            var db = new ApplicationDbContext();
+            var followers = (from s in db.Followers
+                         where s.following == userID
+                         select s).ToList();
+            List<ApplicationUser> result;
+
+            foreach(var s in followers)
+            {
+                var person = (from b in db.Users
+                             where Int32.Parse(b.Id) == s.follower
+                             select b);
+                result.Add(person);
+            }
+
+        }
+        public List<AccountViewModels> getFollowing(int userID)
+        {
+
+        }
     }
 }
