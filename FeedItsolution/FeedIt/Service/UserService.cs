a1d0161e240144d6fffc9af9306508bad0715607
sigmarb@gmail.com <sigmarb@gmail.com>
2015-05-07 12:25:02 +0000

    serivice

--- FeedItsolution/FeedIt/Service/UserService.cs
+++ FeedItsolution/FeedIt/Service/UserService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using FeedIt.Models;
+
 namespace FeedIt.Service
 {
     public class UserService
     {
+        /*
+        public void editUser(U userID)
+        {
+            var db = new ApplicationDbContext();
 
+            var edit = (from s in db.Users
+                        where s.Id == userID
+                        select s).SingleOrDefault();
+        }
+        */
+        public void deleteUser(int userID)
+        {
+            
+        }
     }
 }
