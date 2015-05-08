e41073a855d31ad548273bf749fd04a382ec4bfe
asgeirfri <asgeirfri@gmail.com>
2015-05-08 14:28:56 +0000

    prufa fall i newsfeed

--- FeedItsolution/FeedIt/Controllers/NewsFeedController.cs
+++ FeedItsolution/FeedIt/Controllers/NewsFeedController.cs
@@ -21,6 +21,13 @@ namespace FeedIt.Controllers
             return View(model);
         }
 
+        public List<Post> newsFeedPosts()
+        {
+            string strID = User.Identity.GetUserId();
+            List<Post> model = NewsFeedService.Instance.getFeedForUser(strID);
+            return model;
+        }
+
         public ActionResult groupFeed()
         {
             string strID = User.Identity.GetUserId();
