61a24e205f37e44e3050bc0fedf8e8dc9f9960dd
einarr14 <einarr14@ru.is>
2015-05-10 15:51:56 +0000

    allthisworkfornothing

--- FeedItsolution/FeedIt/Models/IdentityModels.cs
+++ FeedItsolution/FeedIt/Models/IdentityModels.cs
@@ -6,7 +6,6 @@ namespace FeedIt.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string fullName { get; set; }
         public string aboutMe { get; set; }
         public string profilePicture { get; set; }
     }
